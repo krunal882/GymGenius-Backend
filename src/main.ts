dcc9c44d0e68b0f2b859cccae28a91e44037b462
krunal882 <krunalvekariya254@gmail.com>
2024-05-27 22:07:15 +0530

    Added exception filter error handling and user response

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { json, urlencoded } from 'express';
-import * as bodyParser from 'body-parser';
+import { HttpExceptionFilter } from './http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(json());
   app.use(urlencoded({ extended: true }));
-  app.use(bodyParser.raw({ type: 'application/json' }));
 
   app.enableCors({
     origin: 'http://localhost:8081',
@@ -17,7 +16,7 @@ async function bootstrap() {
   });
 
   app.useGlobalPipes(new ValidationPipe());
-
+  app.useGlobalFilters(new HttpExceptionFilter());
   app.enableShutdownHooks();
   await app.init();
 
